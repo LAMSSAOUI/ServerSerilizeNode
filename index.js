8e408b0b805630f68b8758e56e602ffedb689fc7
LAMSSAOUI ABDELALI <lam998216@gmail.com>
2024-03-02 15:47:35 +0100

    delete some file

--- index.js
+++
@@ -1,7 +0,0 @@
-var Stress = require('ddos-stress');
- 
-// Create new instance of DDoS Stress
-var stress = new Stress();
- 
-// Run stress on server
-stress.run('http://localhost:3000',100);
--- models/File.js
+++
@@ -1,9 +0,0 @@
-const {DataTypes} = require("sequelize");
-const sequelize = require("../server.js");
-
-const File = sequelize.define("File", {
-    filename : DataTypes.STRING,
-    path : DataTypes.STRING
-});
-
-module.exports = File
